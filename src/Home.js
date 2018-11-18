00356905efad0c64f483a56c12d8b500442b9299
Joseph Masongsong <joseph@josephmasongsong.com>
2018-11-18 12:04:21 -0800

    querying prismic works

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import FourColumns from './components/FourColumns';
+import LatestNews from './components/LatestNews';
+
 
 class Home extends Component {
   render() {
@@ -9,6 +11,7 @@ class Home extends Component {
 		<Header headline="Free Social Procurement Tools & Resources" subheader="Capitalize on Buy Social Canada's vast social procurement knowledge base for free. 
               Search through our library of tools, frameworks, and resources."/>
         <FourColumns />
+        <LatestNews />
       </div>
     );
   }
