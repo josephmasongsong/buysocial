43174d3a57e6f6db7c6a11213b2777922d2c0a00
josephmasongsong <joseph@josephmasongsong.com>
2019-11-28 12:02:21 -0800

    added read more to directory

--- src/components/slices/algolia/construction/Hit.js
+++ src/components/slices/algolia/construction/Hit.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import images from '../../../../ThemeImages';
 import { DeviceSize } from '../../../../DeviceSize';
 import styled from 'styled-components'
+import { Button, Collapse } from 'reactstrap'
 
 const HitWrapper = styled.div`
   display: flex;
@@ -31,24 +32,80 @@ const HitBody = styled.div`
   a { text-decoration: underline; }
 `
 
-const Hit = ({ hit, clickHandler }) =>
-  <HitWrapper>
-    {hit.image
-      ?
-      <HitImg src={hit.image} alt={hit.name} className={`border mr-4`}/>
-      :
-      <HitImg src={images.placeholder} alt="Company Logo" className={`border mr-4`}/>
+// const Hit = ({ hit, clickHandler }) =>
+//   <HitWrapper>
+//     {hit.image
+//       ?
+//       <HitImg src={hit.image} alt={hit.name} className={`border mr-4`}/>
+//       :
+//       <HitImg src={images.placeholder} alt="Company Logo" className={`border mr-4`}/>
+//     }
+//     <HitBody>
+//       <h4 className={`mb-1`}
+//         >{hit.name}</h4>
+//       {hit.website && <a className={`mb-2 d-inline-block`} href={`http://` + hit.website} target={`_blank`} >{hit.website}</a> }
+//       {hit.phone && <p className={`mb-0`}>{hit.phone}</p> }
+//       {hit.email && <p className={`mb-0`}>{hit.email}</p> }
+//       {hit.address && <p className={`mb-0`}>{hit.address}</p> }
+//       {hit.description &&
+//
+//       }
+//
+//
+//     </HitBody>
+//     {hit.bsc_certification && <HitCert alt={`Buy Social Canada Certified Supplier`} src={images.logo} /> }
+//   </HitWrapper>
+
+class Hit extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: false
     }
-    <HitBody>
-      <h4 className={`mb-1`}
-        // onClick={(e) => clickHandler(hit,e)} style={{ cursor: "pointer" }}
-        >{hit.name}</h4>
-      {hit.website && <a className={`mb-2 d-inline-block`} href={`http://` + hit.website} target={`_blank`} >{hit.website}</a> }
-      {hit.phone && <p className={`mb-0`}>{hit.phone}</p> }
-      {hit.email && <p className={`mb-0`}>{hit.email}</p> }
-      {hit.address && <p className={`mb-0`}>{hit.address}</p> }
-    </HitBody>
-    {hit.bsc_certification && <HitCert alt={`Buy Social Canada Certified Supplier`} src={images.logo} /> }
-  </HitWrapper>
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle() {
+    this.setState(state => ({
+      isOpen: !this.state.isOpen
+    }));
+  }
+
+  render(){
+    const { hit } = this.props
+    const { isOpen } = this.state
+    return(
+      <HitWrapper>
+        {hit.image
+          ?
+          <HitImg src={hit.image} alt={hit.name} className={`border mr-4`}/>
+          :
+          <HitImg src={images.placeholder} alt="Company Logo" className={`border mr-4`}/>
+        }
+        <HitBody>
+          <h4 className={`mb-1`}
+            >{hit.name}</h4>
+          {hit.website && <a className={`mb-2 d-inline-block`} href={`http://` + hit.website} target={`_blank`} >{hit.website}</a> }
+          {hit.phone && <p className={`mb-0`}>{hit.phone}</p> }
+          {hit.email && <p className={`mb-0`}>{hit.email}</p> }
+          {hit.address && <p className={`mb-0`}><i className="fas fa-map-marker-alt text-warning mr-1"></i><a href={`https://www.google.com/maps/search/?api=1&query=${hit.address}`} style={{ textDecoration: 'none' }} target="_blank">{hit.address}</a></p> }
+          {hit.description &&
+            <div>
+              <Button color="link" className="px-0" onClick={this.toggle} style={{ textDecoration: 'underline' }}>
+                {this.state.isOpen ? 'Show Less' : 'Read More'}
+              </Button>
+              <Collapse isOpen={isOpen}>
+                <div className="p-3 bg-light">{hit.description}</div>
+              </Collapse>
+            </div>
+          }
+
+        </HitBody>
+        {hit.bsc_certification && <HitCert alt={`Buy Social Canada Certified Supplier`} src={images.logo} /> }
+      </HitWrapper>
+    )
+  }
+}
+
 
 export default Hit
